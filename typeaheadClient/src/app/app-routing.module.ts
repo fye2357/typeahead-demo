c8427d05bc0c25e8ddc430edb7f31488385f39f6
fye2357 <yangfangyuan@pku.edu.cn>
2023-06-05 20:46:02 -0400

    modify routing

--- typeaheadClient/src/app/app-routing.module.ts
+++ typeaheadClient/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { StateSearchComponent } from './state-search/state-search.component'
+const routes: Routes = [
+  {
+    path: '',
+    component: StateSearchComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
